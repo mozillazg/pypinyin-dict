1725a46c280dc4960475b875e97df9f2ca28d1e3
mozillazg <mozillazg101@gmail.com>
2021-09-20 13:11:57 +0800

    add tests

---
+++ tests/test_load.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from pypinyin_dict.pinyin_data import (
+    cc_cedict as single_cc_cedict,
+    khanyupinlu,
+    khanyupinyin,
+    kmandarin_8105,
+    ktghz2013,
+    kxhc1983,
+    pinyin as single_pinyin,
+    zdic
+)
+from pypinyin_dict.phrase_pinyin_data import (
+    cc_cedict as phrase_cc_cedict,
+    large_pinyin,
+    pinyin as phrase_pinyin,
+    zdic_cibs,
+    zdic_cybs
+)
+
+
+def test_load_pinyin_data():
+    single_cc_cedict.load()
+    khanyupinyin.load()
+    khanyupinlu.load()
+    kmandarin_8105.load()
+    ktghz2013.load()
+    kxhc1983.load()
+    single_pinyin.load()
+    zdic.load()
+
+
+def test_load_pharse_pinyin_data():
+    phrase_cc_cedict.load()
+    large_pinyin.load()
+    phrase_pinyin.load()
+    zdic_cibs.load()
+    zdic_cybs.load()
